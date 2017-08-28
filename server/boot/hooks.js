f68de0dde767b05dd32f76e273e7b93937964ac2
Jyotsna <jyotsna.j.oberai123@gmail.com>
2017-08-28 17:45:09 +0530

    hooks

--- server/boot/hooks.js
+++ server/boot/hooks.js
@@ -10,16 +10,16 @@ module.exports = function(app, cb) {
   function createModel(cb) {
     console.log('\n----- Creating a model -----');
     app.models.Person.create({
-      name: 'Jyotsna Oberai',
-      address: 'Sion',
-      phoneNumber: 7765434456,
-      city: 'Mumbai'
+      name: 'a name',
+      address: 'random value',
+      phoneNumber: 00000000000,
+      city: 'any city'
     }, cb);
   }
 
   function updateModel(coffeeShop, cb) {
     console.log('\n----- Updating a model -----');
-    coffeeShop.updateAttribute('address', 'Vashi', cb);
+    coffeeShop.updateAttribute('address', '', cb);
   }
 
   function destroyModel(coffeeShop, cb) {
